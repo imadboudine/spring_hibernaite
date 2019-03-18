813ac25e42b7309fe62445bf6620c558ce386a62
imad <imad.boudine@gmail.com>
2019-03-18 10:08:05 +0100

    ajout de la méthode findById

---
+++ SpringExample/src/main/java/com/mkyong/stock/bo/StockBo.java
@@ -0,0 +1,17 @@
+package com.mkyong.stock.bo;
+
+import com.mkyong.stock.model.Stock;
+
+public interface StockBo {
+	
+	void String findById() ;
+	
+	void save(Stock stock);
+	
+	void update(Stock stock);
+	
+	void delete(Stock stock);
+	
+	Stock findByStockCode(String stockCode);
+
+}
